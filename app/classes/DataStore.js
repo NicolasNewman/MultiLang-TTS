c6ef8bc338bb068f54b266d1b07909c10c06e248
NicolasNewman <nanewman01@gmail.com>
2019-07-26 15:56:27 -0500

    added electron-store and created a wrapper for it

---
+++ app/classes/DataStore.js
@@ -0,0 +1,36 @@
+import * as Store from 'electron-store';
+
+export default class DataStore {
+    constructor() {
+        this.schema = {
+            key: {
+                type: 'string',
+                description:
+                    'The API key used to authenticate with Google cloud'
+            },
+            defaultPath: {
+                type: 'string',
+                description:
+                    'The default path to open to when the program launches'
+            },
+            uiMode: {
+                type: 'string',
+                enum: ['light', 'dark'],
+                default: 'light',
+                description: 'The targeted theme for the UI'
+            }
+        };
+        this.store = new Store({ schema: this.schema });
+    }
+
+    set = (key, value) => {
+        if (this.schema[key]) {
+            console.log('contains key ', key);
+            this.store.set(key, value);
+        }
+    };
+
+    get = key => {
+        return this.schema[key] ? this.store.get(key) : undefined;
+    };
+}
