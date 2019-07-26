11a793745a587af3b69fb494b29716e06952e45d
NicolasNewman <nanewman01@gmail.com>
2019-07-26 15:56:55 -0500

    integrated Store into settings

--- app/components/Settings/Settings.jsx
+++ app/components/Settings/Settings.jsx
@@ -1,23 +1,67 @@
 import * as React from 'react';
+import DataStore from '../../classes/DataStore';
 import { Button, Input, Typography } from 'antd';
+import routes from '../../constants/routes.json';
 
 const { Text } = Typography;
 
 export default class Settings extends React.Component {
+    constructor(props) {
+        super(props);
+        this.dataStore = new DataStore();
+        const key = this.dataStore.get('key');
+        const defaultPath = this.dataStore.get('defaultPath');
+        console.log(key);
+        console.log(defaultPath);
+        this.state = {
+            key: key ? key : '',
+            defaultPath: defaultPath ? defaultPath : ''
+        };
+    }
+
+    fieldChanged = (value, e) => {
+        const state = {};
+        state[value] = e.target.value;
+        this.setState(state);
+    };
+    saveClicked = () => {
+        this.dataStore.set('key', this.state.key);
+        this.dataStore.set('defaultPath', this.state.defaultPath);
+
+        // this.props.history.push(routes.HOME_MEDIA);
+    };
+
+    cancelClicked = () => {
+        this.props.history.push(routes.HOME_MEDIA);
+    };
     render() {
+        console.log(this.dataStore);
+        console.log(this);
         return (
             <div className="settings">
                 <div className="row settings__group">
                     <Text>API Key: </Text>
-                    <Input placeholder="key" />
+                    <Input
+                        onChange={e => this.fieldChanged('key', e)}
+                        placeholder="key"
+                        value={this.state.key}
+                    />
                 </div>
                 <div className="row settings__group">
-                    <Text>API Key: </Text>
-                    <Input placeholder="key" />
+                    <Text>Default Path: </Text>
+                    <Input
+                        onChange={e => this.fieldChanged('defaultPath', e)}
+                        placeholder="path"
+                        value={this.state.defaultPath}
+                    />
                 </div>
                 <div className="row">
-                    <Button type="primary">Save</Button>
-                    <Button type="default">Cancel</Button>
+                    <Button onClick={this.saveClicked} type="primary">
+                        Save
+                    </Button>
+                    <Button onClick={this.cancelClicked} type="default">
+                        Cancel
+                    </Button>
                 </div>
             </div>
         );
--- app/containers/SettingsPage.jsx
+++ app/containers/SettingsPage.jsx
@@ -1,9 +1,17 @@
 import * as React from 'react';
 import { Component } from 'react';
+import { connect } from 'react-redux';
 import Settings from '../components/Settings/Settings';
 
-export default class HomePage extends Component {
-    render() {
-        return <Settings />;
-    }
-}
+// class HomePage extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.dataStore = new DataStore();
+//     }
+
+//     render() {
+//         return <Settings dataStore={this.dataStore} />;
+//     }
+// }
+
+export default connect()(Settings);
