f8abe16d6adda7533f5b0fe50e3f95d07ff41491
NicolasNewman <nanewman01@gmail.com>
2019-07-26 12:15:38 -0500

    styled basic settings component

--- app/components/Settings/Settings.jsx
+++ app/components/Settings/Settings.jsx
@@ -1,7 +1,25 @@
 import * as React from 'react';
+import { Button, Input, Typography } from 'antd';
+
+const { Text } = Typography;
 
 export default class Settings extends React.Component {
     render() {
-        return <p>Hello</p>;
+        return (
+            <div className="settings">
+                <div className="row settings__group">
+                    <Text>API Key: </Text>
+                    <Input placeholder="key" />
+                </div>
+                <div className="row settings__group">
+                    <Text>API Key: </Text>
+                    <Input placeholder="key" />
+                </div>
+                <div className="row">
+                    <Button type="primary">Save</Button>
+                    <Button type="default">Cancel</Button>
+                </div>
+            </div>
+        );
     }
 }
